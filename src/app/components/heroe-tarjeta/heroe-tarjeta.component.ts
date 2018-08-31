1532411723d5026f2af304e3c95162e71eb33610
YAM v.m <yavemu@gmail.com>
2018-08-31 15:55:09 -0500

    @Output - ejercicio con EventEmitter y pasando parametros al hijo

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,13 +11,18 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe:any = [];
   @Input() indice:number;
 
-  constructor(private router:Router) { }
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+  constructor(private router:Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit() {
   }
 
   verHeroe(){
-    this.router.navigate( ['/heroe',this.indice] );
+    // this.router.navigate( ['/heroe',this.indice] );
+    this.heroeSeleccionado.emit(this.indice)
   }
 
 }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from './../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +13,15 @@ export class HeroesComponent implements OnInit {
 
   constructor(
     private _heroesService:HeroesService,
+    private router:Router,
   ) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx:number){
+    this.router.navigate( ['/heroe',idx] );
   }
 
 }
